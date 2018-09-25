98468d195bb941547b807c48191a2df64d2dbbc0
Ben Patterson <bmizepatterson@gmail.com>
2018-09-25 16:23:06 -0400

    Start of giggling-bunnies solution

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,53 @@
 "use strict";
 
 // This is the JavaScript file we edit
-var test = function test() {
-  console.log("This is a test.");
-};
+var display = document.getElementById('display');
+var first_num, second_num;
+var state = 'ready';
+var sign;
 
-var test2 = "123";
+function input(digit) {
+  if (digit === '.' && display.innerHTML.search(/\./) >= 0) {
+    return;
+  }
+
+  if (state === 'ready_second' || state === 'solved') {
+    display.innerHTML = digit;
+    state = 'ready';
+  } else {
+    if (display.innerHTML === '0') {
+      display.innerHTML = digit;
+    } else {
+      display.innerHTML += digit;
+    }
+  }
+}
+
+function clear_display() {
+  display.innerHTML = '0';
+}
+
+function operator(current_sign) {
+  first_num = parseFloat(display.innerHTML);
+  console.log('first_num:', first_num);
+  state = 'ready_second';
+  sign = current_sign;
+}
+
+function calculate() {
+  second_num = parseFloat(display.innerHTML);
+  var solution;
+
+  if (sign === '/') {
+    solution = first_num / second_num;
+  } else if (sign === 'x') {
+    solution = first_num * second_num;
+  } else if (sign === '-') {
+    solution = first_num - second_num;
+  } else if (sign === '+') {
+    solution = first_num + second_num;
+  }
+
+  display.innerHTML = solution;
+  state = 'solved';
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,54 @@
 // This is the JavaScript file we edit
 
-let test = () => {
-    console.log("This is a test.");
+let display = document.getElementById('display');
+let first_num, second_num;
+let state = 'ready';
+let sign;
+
+function input(digit) {
+    if (digit === '.' && display.innerHTML.search(/\./) >= 0) {
+        return;
+    }
+    if (state === 'ready_second' || state === 'solved') {
+        display.innerHTML = digit;
+        state = 'ready';
+    } else {
+
+        if (display.innerHTML === '0') {
+            display.innerHTML = digit;    
+        } else {
+            display.innerHTML += digit;
+        }
+    }
+
 }
 
-var test2 = "123";
+function clear_display() {
+    display.innerHTML = '0';
+}
+
+function operator(current_sign) {
+    first_num = parseFloat(display.innerHTML);
+    console.log('first_num:', first_num);
+    state = 'ready_second';
+    sign = current_sign;
+}
+
+function calculate() {
+    second_num = parseFloat(display.innerHTML);
+    let solution;
+
+    if (sign === '/') {
+        solution = first_num / second_num;
+    } else if (sign === 'x') {
+        solution = first_num * second_num;
+    } else if (sign === '-') {
+        solution = first_num - second_num;
+    } else if (sign === '+') {
+        solution = first_num + second_num;
+    }
+
+    display.innerHTML = solution;
+    state = 'solved';
+}
+
