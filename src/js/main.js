62f63b167aa746cd2e9bbce35ba1c67d6f0b1cd1
Ben Patterson <bmizepatterson@gmail.com>
2018-09-26 14:48:29 -0400

    Properly formats solution for display

--- src/js/main.js
+++ src/js/main.js
@@ -4,33 +4,58 @@
 var display = document.getElementById('display');
 var first_num = null;
 var second_num = null;
-var state = 'ready';
-var sign;
+var sign = null;
+
+const READY_FIRST = 1;
+const READY_SECOND = 2;
+const SOLVED = 3;
+const ERROR = 4;
+var calc_state = READY_FIRST;
+
+const READY_FOR_ENTRY = 1;
+const ENTRY_IN_PROGRESS = 2;
+var display_state = READY_FOR_ENTRY;
 
 function input(digit) {
-    if (state == 'ready' && display.innerHTML.length > 10) {
-        return;
-    }
-  if (digit === '.' && display.innerHTML.search(/\./) >= 0) {
+  if (display_state == ENTRY_IN_PROGRESS && display.innerHTML.length > 10) {
     return;
   }
 
-  if (state === 'ready_second' || state === 'solved') {
-    display.innerHTML = digit;
-    // state = 'ready';
-  } else {
-    if (display.innerHTML === '0') {
-      display.innerHTML = digit;
-    } else {
-      display.innerHTML += digit;
+  if (digit === '.') {
+    if (display_state == READY_FOR_ENTRY) {
+      display.innerHTML = "0" + digit;
+      display_state = ENTRY_IN_PROGRESS;
+      return;
     }
+    if (display.innerHTML.search(/\./) >= 0) {
+      return;
+    }
+  }
+
+  if (display_state == READY_FOR_ENTRY) {
+
+    // After typing one digit, switch display_state so future inputs
+    // are appended to the current entry.
+    display.innerHTML = digit;
+    display_state = ENTRY_IN_PROGRESS;
+
+  } else if (display_state == ENTRY_IN_PROGRESS) {
+
+      display.innerHTML += digit;
+
   }
 }
 
 function clear_display() {
   display.innerHTML = '0';
-  state = 'ready';
+  calc_state = READY_FIRST;
+  display_state = READY_FOR_ENTRY;
   first_num = second_num = null;
+  sign = null;
+  document.getElementById('/').style.backgroundColor = 'initial';
+  document.getElementById('x').style.backgroundColor = 'initial';
+  document.getElementById('-').style.backgroundColor = 'initial';
+  document.getElementById('+').style.backgroundColor = 'initial';
   console.log('END OF CLEAR_DISPLAY()');
   debug();
 }
@@ -39,28 +64,27 @@ function operator(current_sign) {
     console.log('BEGINNING OF OPERATOR()');
     debug();
 
-    if (state == 'error') {
+    if (calc_state == ERROR) {
 
         clear_display();
-
-    } else if (state == 'ready') {
-
-        first_num = parseFloat(display.innerHTML);
-        state = 'ready_second';
-        sign = current_sign;
-
-    } else if (state == 'solved') {
-
-        state = 'ready_second';
-        sign = current_sign;
-
-    } else if (state == 'ready_second') {
-
-        calculate();
-        state = 'ready_second';
-        sign = current_sign;
+        return;
 
     }
+
+    if (calc_state == READY_FIRST) {
+
+        first_num = parseFloat(display.innerHTML);
+
+    } else if (calc_state == READY_SECOND) { // && second_num != null) {
+
+        calculate();
+
+    }
+
+    calc_state = READY_SECOND;
+    sign = current_sign;
+
+    display_state = READY_FOR_ENTRY;
     document.getElementById('/').style.backgroundColor = 'initial';
     document.getElementById('x').style.backgroundColor = 'initial';
     document.getElementById('-').style.backgroundColor = 'initial';
@@ -72,7 +96,7 @@ function operator(current_sign) {
 }
 
 function calculate() {
-    if (state == 'error') {
+    if (calc_state == ERROR) {
         clear_display();
         return;
     }
@@ -83,41 +107,88 @@ function calculate() {
 
   var solution;
   var temp_first = first_num.toString();
-  var temp_second = second_num.toString(); //temp_first=temp_first.substring(temp_first.search(/\./)).length;
+  var temp_second = second_num.toString();
 
-  temp_first = temp_first.substring(temp_first.search(/\./) + 1).length;
-  temp_second = temp_second.substring(temp_second.search(/\./) + 1).length;
+  let first_dec_places = 0;
+  let second_dec_places = 0;
+  if (temp_first.search(/\./) > -1) {
+    first_dec_places = temp_first.substring(temp_first.search(/\./) + 1).length;
+  }
+  if (temp_second.search(/\./) > -1) {
+    second_dec_places = temp_second.substring(temp_second.search(/\./) + 1).length;
+  }
   var dec_places = 0;
 
-  if (temp_first > temp_second) {
-    dec_places = temp_first;
+  if (first_dec_places > second_dec_places) {
+    dec_places = first_dec_places;
   } else {
-    dec_places = temp_second;
+    dec_places = second_dec_places;
   }
+
+  temp_first = first_num * Math.pow(10, dec_places);
+  temp_second = second_num * Math.pow(10, dec_places);
+  console.log('temp_first', temp_first);
+  console.log('temp_second', temp_second);
+
 
   if (sign === '/') {
-    solution = first_num / second_num;
+    if (second_num === 0) {
+      throw_error();
+      return;
+    }
+    solution = temp_first / temp_second;
   } else if (sign === 'x') {
-    solution = first_num * second_num;
+    solution = temp_first * temp_second;
   } else if (sign === '-') {
-    solution = first_num - second_num;
+    solution = temp_first - temp_second;
   } else if (sign === '+') {
-    solution = first_num + second_num;
+    solution = temp_first + temp_second;
   }
 
-  solution = solution.toFixed(dec_places);
-  if (solution.length > 10) {
-    display.innerHTML = 'ERROR';
-    first_num = second_num = null;
-    state = 'error';
-    return;
-  }
 
-    solution = parseFloat(solution);
+  solution = solution / Math.pow(10, dec_places);
+
+  // If the number of digits in front of the decimal is too many, throw error
+  // Grab the "integer" part of the solution
+  let temp_str = solution.toString();
+  if (temp_str.length > 9) {
+    console.log('solution', solution);
+    let integer = temp_str.substring(0, temp_str.search(/\./));
+    console.log('integer', integer);
+    if (integer.length > 9) {
+      // Can't fit this number on our display; display ERROR.
+      throw_error();
+      return;
+    }
+    let total_decimals = temp_str.substring(temp_str.search(/\./) + 1);
+    // Limit the total digits in the display to 9
+    let avail_digits = 9;
+    let exp_not = '';
+    // Detect exponential notation
+    if (total_decimals.search("e") >= 0) {
+      // Find the length of the e+XX part
+      exp_not = total_decimals.substring(total_decimals.search("e"));
+      total_decimals = total_decimals.substring(0, total_decimals.search("e"));
+      console.log('total_decimals, step -1', total_decimals);
+      avail_digits -= exp_not.length;
+    }
+    // If found, round digits but keep notation
+    console.log('total_decimals, step 1', total_decimals);
+    let num_decimals = avail_digits - integer.length;
+    if (total_decimals.length > num_decimals) {
+      total_decimals = total_decimals / Math.pow(10, total_decimals.length - num_decimals);
+      total_decimals = Math.round(total_decimals);
+      total_decimals = "" + total_decimals + exp_not;
+    }
+    solution = "" + integer + '.' + total_decimals;
+  }
+  
+  solution = parseFloat(solution);
 
 
   display.innerHTML = solution;
-  state = 'solved';
+  calc_state = SOLVED;
+  display_state = READY_FOR_ENTRY;
   sign = null;
 
   first_num = solution;
@@ -147,5 +218,17 @@ function debug() {
     console.log('   first_num', first_num, typeof first_num);
     console.log('   second_num', second_num, typeof second_num);
     console.log('   sign', sign, typeof sign);
-    console.log('   state', state, typeof state);
+    console.log('   calc_state', calc_state);
+    console.log('   display_state', display_state);
+}
+
+function throw_error() {
+    display.innerHTML = 'ERROR';
+    first_num = second_num = sign = null;
+    calc_state = ERROR;
+    display_state = READY_FOR_ENTRY;
+    document.getElementById('/').style.backgroundColor = 'initial';
+    document.getElementById('x').style.backgroundColor = 'initial';
+    document.getElementById('-').style.backgroundColor = 'initial';
+    document.getElementById('+').style.backgroundColor = 'initial';
 }
