30e541117e7a5ce7e1c673806ad670bc7daa9d67
Ben Patterson <bmizepatterson@gmail.com>
2018-09-26 11:48:55 -0400

    Correct operation when calculations are strung together

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,13 +9,16 @@ var state = 'ready';
 var sign;
 
 function input(digit) {
+  if (state == 'ready' && display.innerHTML.length > 10) {
+    return;
+  }
+
   if (digit === '.' && display.innerHTML.search(/\./) >= 0) {
     return;
   }
 
   if (state === 'ready_second' || state === 'solved') {
-    display.innerHTML = digit;
-    state = 'ready';
+    display.innerHTML = digit; // state = 'ready';
   } else {
     if (display.innerHTML === '0') {
       display.innerHTML = digit;
@@ -29,31 +32,42 @@ function clear_display() {
   display.innerHTML = '0';
   state = 'ready';
   first_num = second_num = null;
+  console.log('END OF CLEAR_DISPLAY()');
+  debug();
 }
 
 function operator(current_sign) {
-  if (first_num == null) {
+  console.log('BEGINNING OF OPERATOR()');
+  debug();
+
+  if (state == 'error') {
+    clear_display();
+  } else if (state == 'ready') {
     first_num = parseFloat(display.innerHTML);
     state = 'ready_second';
     sign = current_sign;
-  } else {
+  } else if (state == 'solved') {
+    state = 'ready_second';
+    sign = current_sign;
+  } else if (state == 'ready_second') {
     calculate();
     state = 'ready_second';
     sign = current_sign;
   }
 
   console.log('END OF OPERATOR()');
-  console.log('first_num', first_num);
-  console.log('second_num', second_num);
-  console.log('sign', sign);
+  debug();
 }
 
 function calculate() {
+  if (state == 'error') {
+    clear_display();
+    return;
+  }
+
   second_num = parseFloat(display.innerHTML);
   console.log('BEGINNING OF CALCULATE()');
-  console.log('first_num', first_num, typeof first_num);
-  console.log('second_num', second_num, typeof second_num);
-  console.log('sign', sign);
+  debug();
   var solution;
   var temp_first = first_num.toString();
   var temp_second = second_num.toString(); //temp_first=temp_first.substring(temp_first.search(/\./)).length;
@@ -78,15 +92,23 @@ function calculate() {
     solution = first_num + second_num;
   }
 
-  solution = parseFloat(solution.toFixed(dec_places));
+  solution = solution.toFixed(dec_places);
+
+  if (solution.length > 10) {
+    display.innerHTML = 'ERROR';
+    first_num = second_num = null;
+    state = 'error';
+    return;
+  }
+
+  solution = parseFloat(solution);
   display.innerHTML = solution;
   state = 'solved';
+  sign = null;
   first_num = solution;
   second_num = null;
   console.log('END OF CALCULATE()');
-  console.log('first_num', first_num, typeof first_num);
-  console.log('second_num', second_num, typeof second_num);
-  console.log('sign', sign);
+  debug();
   console.log('solution', solution, typeof solution);
   return solution;
 }
@@ -97,4 +119,11 @@ function highlight(id) {
 
 function unhighlight(id) {
   document.getElementById(id).style.backgroundColor = 'initial';
+}
+
+function debug() {
+  console.log('   first_num', first_num, typeof first_num);
+  console.log('   second_num', second_num, typeof second_num);
+  console.log('   sign', sign, typeof sign);
+  console.log('   state', state, typeof state);
 }
--- src/js/main.js
+++ src/js/main.js
@@ -8,13 +8,16 @@ var state = 'ready';
 var sign;
 
 function input(digit) {
+    if (state == 'ready' && display.innerHTML.length > 10) {
+        return;
+    }
   if (digit === '.' && display.innerHTML.search(/\./) >= 0) {
     return;
   }
 
   if (state === 'ready_second' || state === 'solved') {
     display.innerHTML = digit;
-    state = 'ready';
+    // state = 'ready';
   } else {
     if (display.innerHTML === '0') {
       display.innerHTML = digit;
@@ -28,31 +31,49 @@ function clear_display() {
   display.innerHTML = '0';
   state = 'ready';
   first_num = second_num = null;
+  console.log('END OF CLEAR_DISPLAY()');
+  debug();
 }
 
 function operator(current_sign) {
-  
-  if (first_num == null) {
-    first_num = parseFloat(display.innerHTML);
-    state = 'ready_second';
-    sign = current_sign;
-  } else {
-    calculate();
-    state = 'ready_second';
-    sign = current_sign;
-  }
+    console.log('BEGINNING OF OPERATOR()');
+    debug();
+
+    if (state == 'error') {
+
+        clear_display();
+
+    } else if (state == 'ready') {
+
+        first_num = parseFloat(display.innerHTML);
+        state = 'ready_second';
+        sign = current_sign;
+
+    } else if (state == 'solved') {
+
+        state = 'ready_second';
+        sign = current_sign;
+
+    } else if (state == 'ready_second') {
+
+        calculate();
+        state = 'ready_second';
+        sign = current_sign;
+
+    }
+
   console.log('END OF OPERATOR()');
-  console.log('first_num', first_num);
-  console.log('second_num', second_num);
-  console.log('sign', sign);
+  debug();
 }
 
 function calculate() {
+    if (state == 'error') {
+        clear_display();
+        return;
+    }
   second_num = parseFloat(display.innerHTML);
   console.log('BEGINNING OF CALCULATE()');
-  console.log('first_num', first_num, typeof first_num);
-  console.log('second_num', second_num, typeof second_num);
-  console.log('sign', sign);
+  debug();
   
 
   var solution;
@@ -79,16 +100,25 @@ function calculate() {
     solution = first_num + second_num;
   }
 
-  solution = parseFloat(solution.toFixed(dec_places));
+  solution = solution.toFixed(dec_places);
+  if (solution.length > 10) {
+    display.innerHTML = 'ERROR';
+    first_num = second_num = null;
+    state = 'error';
+    return;
+  }
+
+    solution = parseFloat(solution);
+
+
   display.innerHTML = solution;
   state = 'solved';
+  sign = null;
 
   first_num = solution;
   second_num = null;
   console.log('END OF CALCULATE()');
-  console.log('first_num', first_num, typeof first_num);
-  console.log('second_num', second_num, typeof second_num);
-  console.log('sign', sign);
+  debug();
   console.log('solution', solution, typeof solution);
 
   return solution;
@@ -101,4 +131,11 @@ function highlight(id) {
 
 function unhighlight(id) {
     document.getElementById(id).style.backgroundColor = 'initial';
+}
+
+function debug() {
+    console.log('   first_num', first_num, typeof first_num);
+    console.log('   second_num', second_num, typeof second_num);
+    console.log('   sign', sign, typeof sign);
+    console.log('   state', state, typeof state);
 }
