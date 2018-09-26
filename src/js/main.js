222e3f8e281d545fc1adc8257e5f6496cbcf1716
Kendra <kendra.jordan8@gmail.com>
2018-09-25 23:49:01 -0400

    I was able to console log a button value

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,38 @@
 // This is the JavaScript file we edit
-
-
-
 function calculate(){
-   let value=document.getElementsById("value");
-   console.log(value);
+var a = document.getElementById("one").value;
+var b = document.getElementById("two").value;
+var c = document.getElementById("three").value;
+var d = document.getElementById("four").value;
+var e =  document.getElementById("five").value;
+var f =  document.getElementById("six").value;
+var g =  document.getElementById("seven").value;
+var h =  document.getElementById("eight").value;
+var i =  document.getElementById("nine").value;
+var j =  document.getElementById("zero").value;
+var k = document.getElementById("reset").value;
+var l =  document.getElementById("space_a").value;
+var m =  document.getElementById("space").value;
+var n ="";
+console.log(a);
+/*switch (document.getElementById()) {
+  case a :;
+  case b :;
+  case c :;
+  case d :;
+  case e :;
+  case f :;
+  case g :;
+  case h :;
+  case i :;
+  case j :;
+  case k :;
+  case l :;
+  case m :;
+    n+a;
+    break;
+  default:
+
+}*/
+//document.getElementById("display").innerHTML = a;
 }
