000c85abefd6c0d29b4c7893341390e853370d76
Awesome Inc <awesomeinc@Awesome-Isaac-MBP.local>
2018-09-26 12:11:26 -0400

    Added functionality to all calculator buttons

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,94 @@
 "use strict";
 
 // This is the JavaScript file we edit
-var test = function test() {
-  console.log("This is a test.");
-};
+var store = [];
 
-var test2 = "123";
+function keyPress(input) {
+  switch (input) {
+    case "0":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
 
-function calculate() {}
+    case "1":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "2":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "3":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "4":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "5":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "6":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "7":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "8":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "9":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "+":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "-":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "/":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "*":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case ".":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("");
+      break;
+
+    case "=":
+      document.getElementById("display").innerHTML = eval(store.join(""));
+      break;
+  }
+}
+
+function calculate(store) {}
+
+function reset() {
+  store = [];
+  document.getElementById("display").innerHTML = "";
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,80 @@
 // This is the JavaScript file we edit
+let store = [];
 
-let test = () => {
-    console.log("This is a test.");
+function keyPress(input) {
+  switch (input) {
+    case "0":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "1":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "2":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "3":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "4":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "5":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "6":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "7":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "8":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "9":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "+":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "-":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "/":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "*":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case ".":
+      store.push(input);
+      document.getElementById("display").innerHTML = store.join("")
+      break;
+    case "=":
+      document.getElementById("display").innerHTML = eval(store.join(""));
+      break;
+  }
+
 }
 
-var test2 = "123";
+function calculate(store) {
 
-function calculate() {
-  
+}
+
+function reset() {
+  store = [];
+  document.getElementById("display").innerHTML = "";
 }
